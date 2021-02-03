37ddabb58bc3304582cf0a34ed6d09cd59faf8d6
Vignesh P <vignesh.p1110@gmail.com>
2021-02-04 00:31:34 +0530

    tweaked spotify producer for openweathermap

---
+++ producer/openweathermap_producer.py
@@ -0,0 +1,45 @@
+"""Produce dumb openweathermap content to 'openweathermap.topic' kafka topic."""
+import asyncio
+import json
+import os
+from collections import namedtuple
+from dataprep.connector import connect
+from kafka import KafkaProducer
+from time import sleep
+
+KAFKA_BROKER_URL = os.environ.get("KAFKA_BROKER_URL")
+TOPIC_NAME = os.environ.get("WEATHER_TOPIC_NAME")
+SLEEP_TIME = int(os.environ.get("SLEEP_TIME", 600))
+
+ApiInfo = namedtuple('ApiInfo', ['name', 'access_token'])
+apiInfo = ApiInfo('openweathermap', '73d0140e5ab3cfac25c117068562e17e')
+
+sc = connect(apiInfo.name,
+             _auth={'access_token': apiInfo.access_token},
+             _concurrency=3)
+
+
+async def get_weather(city):
+    """Get the current weather details for the given city.
+
+    Note - Description returns null for now. 
+    """
+    df_weather = await sc.query("weather", q=city)
+    return df_weather
+
+
+def run():
+    producer = KafkaProducer(
+        bootstrap_servers=KAFKA_BROKER_URL,
+        # Encode all values as JSON
+        value_serializer=lambda x: json.dumps(x).encode('ascii'),
+    )
+
+    while True:
+        current_weather = asyncio.run(get_weather(city="Vancouver"))
+        producer.send(TOPIC_NAME, value=current_weather)
+        sleep(SLEEP_TIME)
+
+
+if __name__ == "__main__":
+    run()
